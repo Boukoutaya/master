f40c180fea456cf7b8b908e31d6fddfc5135fe69
Marwa <marwa.boukoutaya@gmail.com>
2024-07-03 14:13:48 +0200

    Ajoutez des fichiers projet.

---
+++ FirstProject/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    public class Car
+    {
+
+        private string Color
+        { get; set; }
+
+        private string Brand { get; set; }
+    }
+}
